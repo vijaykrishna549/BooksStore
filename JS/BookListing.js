e1d453f2cb03a3bea76ec489e89c8c20dac58630
pvijaykrishna549 <you@example.com>
2021-10-08 16:42:25 +0530

    Added JS File to BookStoreHome for Mapping all Books Coming from Server

---
+++ JS/BookListing.js
@@ -0,0 +1,72 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  const flexContainer1 = document.querySelector('.flex-container1');
+  const user = document.querySelector('.user')
+  const wishBtn = document.querySelector('.wishBtn')
+  const userOptions = document.querySelector('.userOptions')
+  const cart = document.querySelector('.cart')
+
+  window.onclick = e => {
+    console.dir(e.target);  // use this in chrome
+    // console.log(e.target);  // use this in firefox - click on tag name to view 
+  }
+
+  user.addEventListener('click', function () {
+    console.log("user")
+    userOptions.style.display = "flex";
+
+})
+
+wishBtn.addEventListener('click', function () {
+    window.location = "http://localhost:5500/Pages/wishlist.html";
+
+})
+
+cart.addEventListener('click', function(){
+  window.location = "http://localhost:5500/Pages/cart.html"
+})
+
+
+
+  requirejs(['../service/dataService.js'], (methods) => {
+    methods.getBooks().then(function (getResponse) {
+      let res = getResponse.data.result;
+      console.log(getResponse)
+      console.log(res)
+
+      flexContainer1.innerHTML = res.map(function (book) {
+        return `<div class="bookContainer" id=${book._id}> 
+            <div class="imgContainer" >
+            <div class="book"id=${book._id}><img src="/Assets/bookListing.png" class ="img"> </div>
+            </div> 
+            <div class="bookName"id=${book._id}>${book.bookName}</div>
+            <div class="author"id=${book._id}>${book.author}</div>
+            <div class="rating">
+            <div class="star" id=${book._id}>4.5*</div>
+            <div class="starRating" id=${book._id}>(20)</div>
+            </div>
+            <div class="price">
+            <div class="price1"id=${book._id}>Rs. ${book.discountPrice}</div>
+            <div class="originalPrice"id=${book.price}> ${book.price}</div>
+            </div>
+            </div>`;
+
+      }).join('');
+
+    })
+
+  });
+  $(document).on('click', '.bookContainer', function (event) {
+    console.log(event.target.id)
+    console.log(event.target.textContent)
+
+    window.location = `http://localhost:5500/Pages/displayBook.html?id=${event.target.id}`
+  })
+
+
+  user.addEventListener('click', function () {
+    console.log("user")
+    userOptions.style.display = "flex";
+
+  })
+})
